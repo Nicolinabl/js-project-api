c02207ff0c792d1fe589097fb74f2de68d13dcde
nicolinabl <nicolina.backlin@telia.com>
2026-01-22 13:51:20 +0100

    return single message according to id. Error message displayed if id does not exist

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import cors from "cors"
 import express from "express"
-import data from "./data.json"
+import data from "./data.json" with { type: "json" }
+import listEndpoints from "express-list-endpoints"
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -14,7 +13,11 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+  const endpoints = listEndpoints(app)
+  res.json({
+    message: "Welcome to tha happy thoughts API",
+    endpoints: endpoints,
+  })
 })
 
 // returns all messages in array
@@ -32,6 +35,17 @@ app.get("/messages/liked", (req, res) => {
 app.get("/messages/happy", (req, res) => {
   let happyMessages = data.filter(message => message.message.toLowerCase().includes("happy"))
   res.json(happyMessages)
+})
+
+// returns a single message with specific id, displays error message if id does not exist
+app.get("/messages/:id", (req, res) => {
+  const message = data.find(message => message._id === req.params.id)
+
+  if (!message) {
+    return res.status(404).json({ error: "no message with that id" })
+  }
+
+  res.json(message)
 })
 
 // Start the server
