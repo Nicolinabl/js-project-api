b85aa64e43236c73096fca35a9b26dd3d72274d9
nicolinabl <nicolina.backlin@telia.com>
2026-01-28 14:49:47 +0100

    post request, post message

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import cors from "cors"
-import express from "express"
-import data from "./data.json" with { type: "json" }
+import express, { application } from "express"
+// import data from "./data.json" with { type: "json" }
 import listEndpoints from "express-list-endpoints"
 import mongoose from "mongoose"
 
 // NOTE - add .env file?
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/project-api"
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/messages"
 mongoose.connect(mongoUrl)
 
 const port = process.env.PORT || 8080
@@ -25,13 +25,38 @@ const messageSchema = new mongoose.Schema({
     type: Number,
     default: 0
   },
-  // timestamps?
+  createdAt: {
+    type: Date,
+    default: () => Date.now()
+  }
 })
 
-// Model based on schema
+// Model
 const Message = mongoose.model('Message', messageSchema)
 
-// ROUTES
+if (process.env.RESET_DATABASE) {
+  // create messages in database
+  const seedDatabase = async () => {
+    // Avoid content in api from being duplicated on refresh
+    await Message.deleteMany()
+
+    const testMessage = new Message({
+      message: 'I am testing',
+      hearts: 3
+    })
+    await testMessage.save()
+
+    const testMessageTwo = new Message({
+      message: 'is it working?',
+      hearts: 10
+    })
+    await testMessageTwo.save()
+
+  }
+  seedDatabase()
+}
+
+// ROUTES GET
 app.get("/", (req, res) => {
   const endpoints = listEndpoints(app)
   res.json({
@@ -40,32 +65,20 @@ app.get("/", (req, res) => {
   })
 })
 
-app.get("/messages", (req, res) => {
-  res.json(data)
+// GET all messages
+app.get("/messages", async (req, res) => {
+  const messages = await Message.find()
+  res.json(messages)
 })
 
+// GET liked messages
 app.get("/messages/liked", (req, res) => {
   let likedMessages = data.filter(message => message.hearts > 0)
   
   res.json(likedMessages)
 })
 
-app.get("/messages/happy", (req, res) => {
-  let happyMessages = data.filter(message => message.message.toLowerCase().includes("happy"))
-
-  res.json(happyMessages)
-})
-
-app.get("/messages/:id", (req, res) => {
-  const message = data.find(message => message._id === req.params.id)
-
-  if (!message) {
-    return res.status(404).json({ error: "no message with that id" })
-  }
-
-  res.json(message)
-})
-
+// GET liked messages (query param)
 app.get("/hearts", (req, res) => {
   let result = data
 
@@ -76,7 +89,54 @@ app.get("/hearts", (req, res) => {
   res.json(result)
  })
 
+// GET messages including word happy
+app.get("/messages/happy", (req, res) => {
+  let happyMessages = data.filter(message => message.message.toLowerCase().includes("happy"))
+
+  res.json(happyMessages)
+})
+
+// GET a single message
+app.get("/messages/:id", (req, res) => {
+  const message = data.find(message => message._id === req.params.id)
+
+  if (!message) {
+    return res.status(404).json({ error: "no message with that id" })
+  }
+
+  res.json(message)
+})
+
+// ROUTES POST
+
+// TODO: Liking a thought
+
+// POST a message (authenticated?)
+app.post('/messages', async (req, res) => {
+  const message = new Message({
+    message: req.body.message,
+    hearts: req.body.hearts
+  })
+  await message.save()
+  res.json(message)
+})
+
+// TODO: Update a message (authenticated)
+
+// TODO: Delete a message
+
+// TODO: Signing up
+
+// TODO: signing in
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
+// TODO
+// 1. Connect input (messages) from happy thoughts app to api
+// 2. Your API should validate user input and return appropriate errors if the input is invalid.
+// 3. You should implement error handling for all your routes, with proper response statuses.
+// 4. Your frontend should be updated with the possibility to Update and Delete a thought.
